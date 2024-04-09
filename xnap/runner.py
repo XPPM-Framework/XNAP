00e151369f70bacff5b9f97b56473285a2f62445
be_cracked <becracked@googlemail.com>
2024-04-09 16:13:16 +0200

    Update runner

--- xnap/runner.py
+++ xnap/runner.py
@@ -13,7 +13,42 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
-if __name__ == '__main__':
+
+def explain_trace(trace, log_params, preprocessor, args):
+    process_instance = [*trace[log_params.get("activity_key", "event")].to_list(), "!"]
+    #process_instance = preprocessor.get_random_process_instance(args.rand_lower_bound, args.rand_upper_bound)
+
+    # Skip prediction and explanation for first event
+    predictions = [None]
+    temporal_explanations = [[]]
+    for prefix_index in range(2, len(process_instance)):
+        # next activity prediction
+        predicted_act_class, predicted_act_class_str, target_act_class, target_act_class_str, prefix_words, model, input_encoded, prob_dist = test.test_prefix(args, preprocessor, process_instance, prefix_index, model_path=args.model_path)
+        #print("Prefix: %s; Next activity prediction: %s; Next activity target: %s" % (prefix_index, predicted_act_class, target_act_class_str))
+        #print("Probability Distribution:")
+        #print(prob_dist)
+
+        # compute lrp relevances
+        eps = 0.001  # small positive number
+        bias_factor = 0.0  # recommended value
+        net = LSTM_bidi(args, model, input_encoded)  # load trained LSTM model
+
+        Rx, Rx_rev, R_rest = net.lrp(prefix_words, predicted_act_class, eps, bias_factor)  # perform LRP
+        R_words = np.sum(Rx + Rx_rev, axis=1)  # compute word-level LRP relevances
+        #scores = net.s.copy()  # classification prediction scores
+        #print(R_words)
+        #predictions.append(predicted_act_class_str)
+        #temporal_explanations.append(list(R_words))
+        predictions.append(predicted_act_class_str)
+        temporal_explanations.append(list(R_words))
+
+    return predictions, temporal_explanations
+
+        #prefix_heatmaps = prefix_heatmaps + html_heatmap(prefix_words, R_words) + "<br>"  # create heatmap
+        #browser.display_html(prefix_heatmaps)  # display heatmap
+
+
+def main():
     args = config.load()
     output = utils.load_output()
     utils.clear_measurement_file(args)
@@ -50,34 +85,9 @@ if __name__ == '__main__':
         activity_relevance_explanations = []
         cases = event_log.groupby(log_params.get("case_id_key", "case"))
         for case_id, trace in tqdm(cases, desc="Explain traces"):
-            # Skip prediction and explanation for first event
-            predictions.append(None)
-            temporal_explanations.append([])
-
-            process_instance = [*trace[log_params.get("activity_key", "event")].to_list(), "!"]
-            #process_instance = preprocessor.get_random_process_instance(args.rand_lower_bound, args.rand_upper_bound)
-
-            for prefix_index in range(2, len(process_instance)):
-                # next activity prediction
-                predicted_act_class, predicted_act_class_str, target_act_class, target_act_class_str, prefix_words, model, input_encoded, prob_dist = test.test_prefix(args, preprocessor, process_instance, prefix_index, model_path=args.model_path)
-                print("Prefix: %s; Next activity prediction: %s; Next activity target: %s" % (prefix_index, predicted_act_class, target_act_class_str))
-                print("Probability Distribution:")
-                print(prob_dist)
-
-                # compute lrp relevances
-                eps = 0.001  # small positive number
-                bias_factor = 0.0  # recommended value
-                net = LSTM_bidi(args, model, input_encoded)  # load trained LSTM model
-
-                Rx, Rx_rev, R_rest = net.lrp(prefix_words, predicted_act_class, eps, bias_factor)  # perform LRP
-                R_words = np.sum(Rx + Rx_rev, axis=1)  # compute word-level LRP relevances
-                scores = net.s.copy()  # classification prediction scores
-                print(R_words)
-                predictions.append(predicted_act_class_str)
-                temporal_explanations.append(list(R_words))
-
-                #prefix_heatmaps = prefix_heatmaps + html_heatmap(prefix_words, R_words) + "<br>"  # create heatmap
-                #browser.display_html(prefix_heatmaps)  # display heatmap
+            prediction, temporal_explanation = explain_trace(trace, log_params, preprocessor, args)
+            predictions.append(prediction)
+            temporal_explanations.append(temporal_explanation)
 
         event_log["prediction"] = predictions
         event_log["explanation"] = temporal_explanations
@@ -117,3 +127,7 @@ if __name__ == '__main__':
 
     else:
         print("No mode selected ...")
+
+
+if __name__ == '__main__':
+    main()
